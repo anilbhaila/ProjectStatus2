1dd23cf42d08e0f4c3e49cd940de7adc97b6d54e
Lin Yong <kukoo.github@gmail.com>
2013-06-29 17:30:04 +0800

    Project Init.

---
+++ app/scripts/controllers/ProjectOverview.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ProjectStatus2App')
+  .controller('ProjectOverviewCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
